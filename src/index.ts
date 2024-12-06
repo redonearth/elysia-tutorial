1edf9886e06496d1ba35d03db07ca4f21df64f26
Jisang Hong <wona23@gmail.com>
2024-12-06 13:35:12 +0900

    chore: Swagger 플러그인 추가

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .get("/hi", "👋")
+  .get("/countries/:code", ({ params: { code } }) => ({
+    code,
+  }))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
